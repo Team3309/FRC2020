905eacc8680ce29b8af8ab9cae45808b54842c8c
Mark Ghebrial <mghebrialjr@icloud.com>
2020-01-15 20:49:38 -0800

    Added DriveManual Skeleton (might not compile)

--- src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
@@ -3,7 +3,9 @@ package org.usfirst.frc.team3309.commands.drive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.usfirst.frc.team3309.subsystems.Drive;
 
-public class DriveManual extends CommandBase{
+public class DriveManual extends CommandBase {
+
+    CheesyDriveHelper cheesyDrive = new CheesyDriveHelper();
 
     @Override
     public void initialize() {
@@ -12,7 +14,11 @@ public class DriveManual extends CommandBase{
 
     @Override
     public void execute() {
+        double throttle = 0;
+        double turn = 0;
+        boolean quickturn = false;
 
+        Robot.drive.setLeftRight(cheesyDrive.update(throttle, turn, quickturn, false));
     }
 
     public void end() {
@@ -21,6 +27,6 @@ public class DriveManual extends CommandBase{
 
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
 }
--- src/main/java/org/usfirst/frc/team3309/subsystems/Drive.java
+++ src/main/java/org/usfirst/frc/team3309/subsystems/Drive.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team3309.subsystems;
 
+import org.usfirst.frc.team3309.util.DriveSignal;
+
 public class Drive {
     public Drive() {}
+
+    public void setLeftRight (DriveSignal signal) {
+
+    }
 }
