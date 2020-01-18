2237ca06e0b944eda596add1414a4cbcbf8d9b07
Mark Ghebrial <mghebrialjr@icloud.com>
2020-01-17 19:19:13 -0800

    Updated OI

--- src/main/java/org/usfirst/frc/team3309/OI.java
+++ src/main/java/org/usfirst/frc/team3309/OI.java
@@ -6,11 +6,30 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import org.usfirst.frc.team3309.commands.shooter.*;
 
 public class OI {
+
+    public static final int LEFT_CLUSTER_1_ID = 1;
+    public static final int LEFT_CLUSTER_2_ID = 1;
+    public static final int LEFT_CLUSTER_3_ID = 1;
+    public static final int LEFT_CLUSTER_4_ID = 1;
+    public static final int LEFT_CLUSTER_5_ID = 1;
+    public static final int LEFT_CLUSTER_6_ID = 1;
+    public static final int RIGHT_CLUSTER_1_ID = 1;
+    public static final int RIGHT_CLUSTER_2_ID = 1;
+    public static final int RIGHT_CLUSTER_3_ID = 1;
+    public static final int RIGHT_CLUSTER_4_ID = 1;
+    public static final int RIGHT_CLUSTER_5_ID = 1;
+    public static final int RIGHT_CLUSTER_6_ID = 1;
+    public static final int UP_BUTTON_ID = 1;
+    public static final int DOWN_BUTTON_ID = 1;
+    public static final int LEFT_BUTTON_ID = 1;
+    public static final int RIGHT_BUTTON_ID = 1;
+    public static final int TRIGGER_ID = 1;
+
     Joystick leftStick = new Joystick(0);
     Joystick rightStick = new Joystick(1);
     XboxController operatorController = new XboxController(2);
 
-    JoystickButton shootingButton = new JoystickButton(leftStick, 1);
+    JoystickButton shootingButton = new JoystickButton(leftStick, TRIGGER_ID);
 
     OI() {
         shootingButton.whenPressed(new FireAuto());
--- src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team3309.commands.drive;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.usfirst.frc.team3309.Robot;
-import org.usfirst.frc.team3309.lib.util.CheesyDriveHelper;
+import org.usfirst.frc.team3309.util.CheesyDriveHelper;
 
 public class DriveManual extends CommandBase {
 
