c8a5b0269a857da45822af53903713ca65a3043f
Mark Ghebrial <mghebrialjr@icloud.com>
2020-01-23 17:46:45 -0800

    Fixed a mistake

--- src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
+++ src/main/java/org/usfirst/frc/team3309/commands/drive/DriveManual.java
@@ -1,21 +1,15 @@
 package org.usfirst.frc.team3309.commands.drive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import org.usfirst.frc.team3309.Constants;
 import org.usfirst.frc.team3309.Robot;
 import org.usfirst.frc.team3309.subsystems.Drive;
 import org.usfirst.frc.team3309.util.CheesyDriveHelper;
 
 public class DriveManual extends CommandBase {
 
-    // Creating my kinematics object: track width of 27 inches
-    DifferentialDriveKinematics kinematics =
-            new DifferentialDriveKinematics(Units.inchesToMeters(Constants.DRIVETRAIN_WIDTH));
+
+    CheesyDriveHelper cheesyDrive = new CheesyDriveHelper();
 
     @Override
     public void initialize() {
@@ -24,19 +18,14 @@ public class DriveManual extends CommandBase {
 
     @Override
     public void execute() {
+
         if(!hasRequirement(Robot.drive)) return;
 
         double throttle = Robot.oi.leftStick.getX();
         double turn = Robot.oi.rightStick.getY();
+        boolean quickturn = Robot.oi.rightStick.getTrigger();
 
-        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(throttle, 0, turn);
-
-        // Convert to wheel speeds
-        DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
-
-        Robot.drive.setLeftRight(ControlMode.PercentOutput,
-                wheelSpeeds.leftMetersPerSecond,
-                wheelSpeeds.rightMetersPerSecond);
+        Robot.drive.setLeftRight(ControlMode.PercentOutput, cheesyDrive.update(throttle, turn, quickturn, false));
     }
 
     public void end() {
