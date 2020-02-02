9c9af9434a42a348ea70506d6ce179fd262e2f43
Joshua Badzey <themaninthesnowboots@gmail.com>
2020-02-01 18:36:48 -0800

    Restructured RobotContainer and Robot to match the examples given by WPI.

--- src/main/java/org/usfirst/frc/team3309/RobotContainer.java
+++ src/main/java/org/usfirst/frc/team3309/RobotContainer.java
@@ -1,7 +1,14 @@
 package org.usfirst.frc.team3309;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
 import org.usfirst.frc.team3309.subsystems.*;
 
+/*
+* A class that contains all the subsystems and commands that Robot needs. Based off of the RobotContainer
+* example class by WPI.
+*
+*/
 public class RobotContainer {
 
     //Constructs instances of our subsystems.
@@ -13,6 +20,7 @@ public class RobotContainer {
     private final ShooterSubsystem Shooter = new ShooterSubsystem();
     private final VisionSubsystem Vision = new VisionSubsystem();
 
+    SendableChooser<Command> chooser = new SendableChooser<>();
     public RobotContainer() {
 
         ConfigureButtonBindings();
@@ -20,5 +28,12 @@ public class RobotContainer {
     }
 
     private void ConfigureButtonBindings() {
+
+
+
+    }
+
+    public Command GetAutonomousCommand() {
+        return chooser.getSelected();
     }
 }
