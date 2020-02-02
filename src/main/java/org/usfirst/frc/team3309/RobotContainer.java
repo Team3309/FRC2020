65656c8f9610502325b3a30a32b5d6230469d61e
Josh Leyshock <digital.zilla@gmail.com>
2020-02-01 18:49:04 -0800

    two example commands using the new command library, one set up as a command class, and one exactly the same but as an inline RunCommand

--- src/main/java/org/usfirst/frc/team3309/RobotContainer.java
+++ src/main/java/org/usfirst/frc/team3309/RobotContainer.java
@@ -1,6 +1,16 @@
 package org.usfirst.frc.team3309;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import org.usfirst.frc.team3309.commands.drive.DriveManual;
+import org.usfirst.frc.team3309.commands.drive.DriveSimpleTest;
 import org.usfirst.frc.team3309.subsystems.*;
+
+import javax.naming.ldap.Control;
 
 public class RobotContainer {
 
@@ -16,9 +26,21 @@ public class RobotContainer {
     public RobotContainer() {
 
         ConfigureButtonBindings();
+        SetDefaultCommands();
 
     }
 
+
+    private void SetDefaultCommands() {
+        CommandScheduler.getInstance().setDefaultCommand(Drive, new DriveManual());
+    }
+
+
     private void ConfigureButtonBindings() {
+        new JoystickButton(Robot.oi.operatorController, XboxController.Button.kA.value)
+                .whileHeld(new DriveSimpleTest(0.1, Drive));
+
+        Button button = new JoystickButton(Robot.oi.operatorController, XboxController.Button.kB.value)
+                .whileHeld(new RunCommand(() -> Drive.SetLeftRight(ControlMode.PercentOutput, 0.2, 0.2), Drive));
     }
 }
