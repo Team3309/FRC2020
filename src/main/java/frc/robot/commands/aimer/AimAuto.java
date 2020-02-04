ae795b42d8b2c8d646850fed6c758539d6dd2570
Mark Ghebrial <mghebrialjr@icloud.com>
2020-02-04 15:40:53 -0800

    Updated AimAuto

--- src/main/java/frc/robot/commands/aimer/AimAuto.java
+++ src/main/java/frc/robot/commands/aimer/AimAuto.java
@@ -1,30 +1,40 @@
 package frc.robot.commands.aimer;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 public class AimAuto extends CommandBase {
 
+    private DriveSubsystem Drive;
+    private VisionSubsystem Vision;
+
     PIDController turnPID;
+
+    public AimAuto (DriveSubsystem drive, VisionSubsystem vision) {
+        Drive = drive;
+        Vision = vision;
+    }
 
     @Override
     public void initialize() {
         turnPID = new PIDController(Constants.kAimingP, Constants.kAimingI, Constants.kAimingD);
 
-
+        addRequirements();
     }
 
     @Override
     public void execute() {
         double turn = 0;
 
-        //TODO: Update with RobotContainer DI
-//        if (Robot.vision.limelight.hasTarget()) {
-//            turn = turnPID.calculate(Robot.vision.limelight.getTx(), 0);
-//        }
-//
-//        Robot.drive.setLeftRight(ControlMode.PercentOutput, -turn, turn);
+        if (Vision.limelight.HasTarget()) {
+            turn = turnPID.calculate(Vision.limelight.GetTx(), 0);
+        }
+
+        Drive.SetLeftRight(ControlMode.PercentOutput, -turn, turn);
 
     }
 
