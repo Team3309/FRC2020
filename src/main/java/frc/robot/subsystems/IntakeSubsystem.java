4465d5302dcb12ab2e9091cdc78bf48e08f4a43e
Mark Ghebrial <mghebrialjr@icloud.com>
2020-02-05 19:29:03 -0800

    Added commands to extend/retract the intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Config;
 import frc.robot.RobotContainer;
@@ -17,15 +18,24 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public RobotContainer robotContainer;
     public WPI_TalonFX intakeMotor;
+    public Solenoid solenoid;
 
     public IntakeSubsystem(RobotContainer container) {
         intakeMotor = new WPI_TalonFX(Config.IntakeMotorID);
         intakeMotor.configFactoryDefault();
         robotContainer = container;
+        solenoid = new Solenoid(Config.IntakeSoleoidChannel);
     }
 
-    public void Actuate(double speed) {
-        intakeMotor.set(ControlMode.Velocity, speed);
+    public void Spin(double speed) {
+        intakeMotor.set(ControlMode.PercentOutput, speed);
     }
 
+    public void Extend() {
+        solenoid.set(true);
+    }
+
+    public void Retract() {
+        solenoid.set(false);
+    }
 }
