e775092157367c35e661633547e402a1267e12ad
Tim Kavner <tjkavner@gmail.com>
2020-02-08 17:22:37 -0800

    Modified shootersubsystem

---
+++ src/main/java/frc/robot/commands/groups/SingleShotCommandGroup.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.groups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.commands.UpdateState;
+import frc.robot.commands.intake.Retract;
+import frc.robot.commands.pcindexer.LoadBall;
+import frc.robot.commands.shooter.PrepareToFire;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class SingleShotCommandGroup extends SequentialCommandGroup {
+    public SingleShotCommandGroup(ShooterSubsystem shooter, IndexerSubsystem indexer, IntakeSubsystem intake) {
+        addCommands(
+                new UpdateState(RobotContainer.PowerCellHandlingState.SINGLE_SHOT),
+                new Retract(intake),
+                new PrepareToFire(shooter),
+                new LoadBall(indexer),
+                new UpdateState(RobotContainer.PowerCellHandlingState.INIT_READY_TO_SHOOT)
+        );
+
+    }
+}
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -18,6 +18,16 @@ import frc.robot.Config;
 
 public class ShooterSubsystem extends SubsystemBase {
 
+     /**
+      * Flywheel
+      */
+    private double flywheelSpeedTop;
+    private double flywheelSpeedBottom;
+
+     public boolean isFlywheelToSpeed() {
+         return Math.abs(GetTopMotorVelocity() - flywheelSpeedTop) < 0.01 &&
+                 Math.abs(GetTopMotorVelocity() - flywheelSpeedTop) < 0.01;
+     }
 
     private WPI_TalonFX topMotor;
     private WPI_TalonFX bottomMotor;
@@ -38,8 +48,7 @@ public class ShooterSubsystem extends SubsystemBase {
      * Configures shooter motors to shooter constants, including PID values, ramp rates and motor settings.
      *
      * @param talon - the talon to be configured.
-     *
-     \----------------------------------------------------------------------------------------------------------------*/
+     */
     public void ConfigTalon(WPI_TalonFX talon) {
 
         talon.configFactoryDefault();;
@@ -60,18 +69,15 @@ public class ShooterSubsystem extends SubsystemBase {
 
      /**---------------------------------------------------------------------------------------------------------------\
      * Spins up the flywheels in preparation for firing.
-     *
-     * @param speed - the speed at which the shooter flywheels will turn.
-     *
-     \----------------------------------------------------------------------------------------------------------------*/
-    public void SpinUpFlywheels(double speed) {
-            SpinPowerCell(speed, -speed);
+     */
+    public void SpinUpFlywheels() {
+
+        SpinPowerCell(flywheelSpeedTop, flywheelSpeedBottom);
     }
 
      /**---------------------------------------------------------------------------------------------------------------\
      * Stops the flywheels by having them slow down to a complete stop.
-     *
-     \----------------------------------------------------------------------------------------------------------------*/
+     */
     public void StopFlywheels() {
         topMotor.set(ControlMode.PercentOutput, 0);
         bottomMotor.set(ControlMode.PercentOutput, 0);
@@ -83,12 +89,18 @@ public class ShooterSubsystem extends SubsystemBase {
      *
      * @param topSpeed - the desired speed of the top motor.
      * @param bottomSpeed - the desired speed of the bottom motor.
-     *
-     \----------------------------------------------------------------------------------------------------------------*/
+     */
     public void SpinPowerCell(double topSpeed, double bottomSpeed) {
         topMotor.set(ControlMode.Velocity, topSpeed);
         bottomMotor.set(ControlMode.Velocity, bottomSpeed);
     }
+     /**---------------------------------------------------------------------------------------------------------------\
+      * Sets motor speed for both motors equally
+      */
+     public void setDesiredSpeed(double speed) {
+         flywheelSpeedTop = speed;
+         flywheelSpeedBottom = speed;
+     }
 
      /**---------------------------------------------------------------------------------------------------------------\
      * Returns the top motor's current speed.
