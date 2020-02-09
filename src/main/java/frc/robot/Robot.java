fde61dc76cc8db7acbd3cb02c73a4fbe507489e7
Josh Leyshock <digital.zilla@gmail.com>
2020-02-08 18:58:04 -0800

    Incorrectly merged.  When in the 3 pane view, it doesn't auto-merge non-conflicts.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,11 +31,12 @@ public class Robot extends TimedRobot {
     public Robot() {
         super(0.01);
     }
-    /*
+
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called when the Robot program starts. use it to initialize your subsystems,
      * and to set up anything that needs to be initialized with the robot.
      */
-
     @Override
     public void robotInit() {
 
@@ -43,7 +44,8 @@ public class Robot extends TimedRobot {
         ContainerTest = new RobotContainerTest();
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called when the Robot enters disabled.
      * It should be used to shut down processes that should only run when the bot is sendIsEnabled.
      */
@@ -51,7 +53,8 @@ public class Robot extends TimedRobot {
     public void disabledInit() {
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called when the Robot enters autonomous.
      * It should be used to get the current auto mode, and launch the appropriate autonomous mode.
      */
@@ -64,7 +67,8 @@ public class Robot extends TimedRobot {
         }
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called every 20 milliseconds while the robot is in autonomous.
      * It should be used to perform periodic tasks that need to be done while the robot is in autonomous.
      */
@@ -72,7 +76,8 @@ public class Robot extends TimedRobot {
     public void autonomousPeriodic() {
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called when the Robot enters teleop.
      * It should be used to shut down any autonomous code, and prepare the bot for human control.
      */
@@ -83,7 +88,8 @@ public class Robot extends TimedRobot {
         }
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called every 2 milliseconds while the robot is in autonomous.
      * It should be used to perform periodic tasks that need to be done while the robot is in teleop.
      */
@@ -91,7 +97,8 @@ public class Robot extends TimedRobot {
     public void teleopPeriodic() {
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function is called when the Robot enters test mode.
      */
     @Override
@@ -99,11 +106,16 @@ public class Robot extends TimedRobot {
         CommandScheduler.getInstance().cancelAll();
     }
 
+
+    /** ----------------------------------------------------------------------------------------------------------------
+     *
+     */
     @Override
     public void testPeriodic() {
     }
 
-    /*
+
+    /** ----------------------------------------------------------------------------------------------------------------
      * This function always runs, regardless of mode.
      */
     @Override
