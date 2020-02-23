12151e0775148667e8dc9bf2bd640b3c1f95b79c
Doug Kavner <dmkavner@yahoo.com>
2020-02-22 17:29:46 -0800

    Cleanup Waypoint class

--- src/main/java/frc/robot/util/Waypoint.java
+++ src/main/java/frc/robot/util/Waypoint.java
@@ -1,14 +1,11 @@
 package frc.robot.util;
 
-import frc.robot.subsystems.DriveSubsystem;
-
 public class Waypoint {
-    public double downFieldInches = 0; //how far the waypoint is from the driver station
-    public double xFieldInches = 0; //lateral position of the waypoint
-    public double turnRadiusInches = 0; //centered on the vertices of the straight-line path, not the guide circles
-    public boolean reverse = false;  // robot backs into waypoint
+    public double downFieldInches; //how far the waypoint is from the driver station
+    public double xFieldInches; //lateral position of the waypoint
+    public double turnRadiusInches; //centered on the vertices of the straight-line path, not the guide circles
+    public boolean reverse;  // robot backs into waypoint
 
-    //TODO: Tune these
     public double linCreepSpeed = 5; //Inches per second
     public double angCreepSpeedInDegsPerSec = 2;
     public double maxLinearSpeed = 40; //Inches per 100 milliseconds
@@ -17,25 +14,7 @@ public class Waypoint {
     public double linDecelerationInInchesPerSec2 = 160; //Also in inches per 100 milliseconds^2
     public double angAccelerationInDegsPerSec2 = 100;
     public double angDecelerationInDegsPerSec2 = 100;
-    public double angToleranceInEncoderCounts;
-    public double angToleranceInInches;
-    public double linToleranceInEncoderCounts;
     public double linToleranceInInches;
-
-
-
-    public double maxLinSpeedEncoderCtsPer100ms;
-    public double linAccelerationEncoderCtsPer100ms2;
-    public double linDecelerationEncoderCtsPer100ms2;
-    public double linCreepSpeedEncoderCtsPer100ms;
-    public double maxAngSpeedEncoderCtsPer100ms;
-    public double angAccelerationEncoderCtsPer100ms2;
-    public double angDecelerationEncoderCtsPer100ms2;
-    public double angCreepSpeedEncoderCtsPer100ms;
-
-    public Waypoint() {
-        initialize();
-    }
 
     public Waypoint(double downFieldInches,
                     double xFieldInches,
@@ -45,8 +24,6 @@ public class Waypoint {
         this.xFieldInches = xFieldInches;
         this.turnRadiusInches = turnRadiusInches;
         this.reverse = reverse;
-
-        initialize();
     }
 
     public Waypoint(double downFieldInches,
@@ -65,18 +42,5 @@ public class Waypoint {
         this.linCreepSpeed = linCreepSpeed;
         this.angCreepSpeedInDegsPerSec = angularCreepSpeed;
         this.reverse = reverse;
-
-        initialize();
-    }
-
-    private void initialize () {
-        maxLinSpeedEncoderCtsPer100ms = DriveSubsystem.inchesPerSecondToEncoderVelocity(maxLinearSpeed);
-        linAccelerationEncoderCtsPer100ms2 = DriveSubsystem.inchesPerSecondToEncoderVelocity(linAccelerationInInchesPerSec2);
-        linDecelerationEncoderCtsPer100ms2 = DriveSubsystem.inchesPerSecondToEncoderVelocity(linDecelerationInInchesPerSec2);
-        linCreepSpeedEncoderCtsPer100ms = DriveSubsystem.inchesPerSecondToEncoderVelocity(linCreepSpeed);
-        maxAngSpeedEncoderCtsPer100ms = DriveSubsystem.degreesPerSecondToEncoderVelocity(maxAngularSpeedInDegsPerSec);
-        angAccelerationEncoderCtsPer100ms2 = DriveSubsystem.degreesPerSecondToEncoderVelocity(angAccelerationInDegsPerSec2);
-        angDecelerationEncoderCtsPer100ms2 = DriveSubsystem.degreesPerSecondToEncoderVelocity(angDecelerationInDegsPerSec2);
-        angCreepSpeedEncoderCtsPer100ms = DriveSubsystem.degreesPerSecondToEncoderVelocity(angCreepSpeedInDegsPerSec);
     }
 }
