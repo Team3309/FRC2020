31634c2d5a5cf92116a7ba69ac6f5b077048acec
Josh Leyshock <digital.zilla@gmail.com>
2020-02-26 22:46:02 -0800

    Didn't realize these values could be null.

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -343,15 +343,15 @@ public class Config {
                 alpha - gamma = chain slack taut to taut = 3984
                 alpha - beta = run out after hitting battery case = 1881
                  */
-                //armPositionVision = 42900; //if you update this then you also need to update the limelightMountingAngle
+                armPositionVision = -20000; //if you update this then you also need to update the limelightMountingAngle
                 maxArmPosition = 0;
-                //trenchArmPosition = 6288;
+                trenchArmPosition = -50000;
                 minArmPosition = -69000;
                 armPositionIntakeStowedLimit = -52500;
                 armPositionIntakeStowedTarget = armPositionIntakeStowedLimit + armPositioningTolerance;
                 armPositionIntakeStowedUpperLimit = armPositionIntakeStowedTarget + armPositioningTolerance;
-                //armControlPanelPosition = 74500; //this needs to be bigger than the hard stop
-                //armPositionHardStop = 74000;
+                armControlPanelPosition = 0; //this needs to be bigger than the hard stop
+                armPositionHardStop = 0;
                 limelightMountingAngle = -5.0;
                 limelightMountingHeight = 33.0; //inches
                 fieldVisionTargetHeight = 78.0 + 14.375; //inches
