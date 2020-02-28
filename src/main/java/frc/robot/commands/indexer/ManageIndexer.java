f31a5772d03d29783213b75d3399c0aebdeefa6a
Doug Kavner <dmkavner@yahoo.com>
2020-02-28 04:33:58 -0800

    Fix auto index in not stopping at max power cells

--- src/main/java/frc/robot/commands/indexer/ManageIndexer.java
+++ src/main/java/frc/robot/commands/indexer/ManageIndexer.java
@@ -5,10 +5,10 @@ import frc.robot.Config;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
+//default command of indexer
 public class ManageIndexer extends CommandBase {
 
     private final ShooterSubsystem shooter;
-    //default command of indexer.
     private IndexerSubsystem Indexer;
     private boolean wasToSpeed;
     private double flywheelSpeedAverage;
@@ -27,17 +27,17 @@ public class ManageIndexer extends CommandBase {
     public void execute() {
         if (shooter.areFlywheelsToSpeed()) {
             wasToSpeed = true;
-            flywheelSpeedAverage = shooter.getTopMotorVelocity() / 2 + shooter.getBottomMotorVelocity() / 2;
+            flywheelSpeedAverage = (shooter.getTopMotorVelocity() + shooter.getBottomMotorVelocity()) / 2;
         }
         //we aren't being given any sensor in the subsystem to determine
         //if there is a ball in the way so we need to figure
         //that out here using the shooter wheel now
         //basically if the flywheel average speed is below a threshold, index in.
-        //use average to avoid errors as we use power output mode on shooter intaking.
-        if (Indexer.getCount() <= Config.maxPowerCells)  {
+        //use average to avoid errors as we use percent output mode on shooter intaking.
+        if (Indexer.getCount() < Config.maxPowerCells)  {
             if (wasToSpeed && (shooter.getTopMotorVelocity() + shooter.getBottomMotorVelocity()) / 2 < flywheelSpeedAverage * Config.indexerPowerCellNeedsIndexingThreshold) {
                 wasToSpeed = false; //reset this so we don't repeatedly index in.
-                //Indexer.indexIn(); // TODO BUG: this keeps going once detected without stopping
+                Indexer.indexIn();
             }
         }
     }
