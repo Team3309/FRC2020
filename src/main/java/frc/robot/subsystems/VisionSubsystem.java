bb3fb5f30df4f5092526d6f5262e03af02522cb2
Doug Kavner <dmkavner@yahoo.com>
2020-03-07 21:00:42 -0800

    Collapse code

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,8 +6,6 @@ import frc.robot.Config;
 import frc.robot.util.Limelight;
 
 /**
- * @author Joshua Badzey
- *
  * The vision class for the vision subsystem, which will perform all Limelight related activities, such
  * as turning the LEDs on and off. Will work with drive to determine accurate drive paths and will also work
  * with aimer and shooter to determine optimal shot distance and power.
@@ -15,7 +13,6 @@ import frc.robot.util.Limelight;
  */
 
 public class VisionSubsystem extends SubsystemBase {
-
 
     public Limelight limelight;
 
@@ -55,9 +52,7 @@ public class VisionSubsystem extends SubsystemBase {
                 Config.visionDistanceConstant * (Config.fieldVisionTargetHeight-Config.limelightMountingHeight) /
                 Math.tan(Math.toRadians(limelight.getTy() + Config.limelightMountingAngle));
 
-
         //now we can convert the new values into a new distance
-
         return distanceToVisionTarget;
     }
 
@@ -73,7 +68,6 @@ public class VisionSubsystem extends SubsystemBase {
      * Sends limelight data to SmartDashboard.
      */
     public void outputToDashboard() {
-        //SmartDashboard.putNumber("Key", value);
 
         double theta = Math.toRadians(getAngleToTarget()); // we rename these for sake of sanity with the math
         double phi = Math.toRadians(getHeightAngleToTarget());
@@ -86,7 +80,6 @@ public class VisionSubsystem extends SubsystemBase {
         double lengthToThreePointGoal = lengthToTarget;
         double heightToThreePointGoal = heightToTarget + Config.fieldVisionHeightOfThreePointHoleFromVisionTarget;
 
-        //
         double threePointHoleDistance = Math.sqrt(depthToThreePointGoal * depthToThreePointGoal + lengthToThreePointGoal * lengthToThreePointGoal + heightToThreePointGoal * heightToThreePointGoal);
         //WE'LL NEED THESE LATER HOLD ON TO THE FORMULAE FOR NOW
         double threePointHoleTx = Math.toDegrees(Math.asin(heightToThreePointGoal / threePointHoleDistance)); //aka adjusted phi, aka the angle we need to rotate by to be facing the 3 point goal
@@ -96,9 +89,7 @@ public class VisionSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Tx (Adjusted)", threePointHoleTx);
         SmartDashboard.putNumber("Ty (Adjusted)", threePointHoleTy);
         SmartDashboard.putNumber("Vision Distance", getDistanceToTarget());
-
         SmartDashboard.putNumber("Distance to Alliance Wall (Depth)", depthToTarget);
-
     }
 
     public double getHeightAngleToTarget() {
